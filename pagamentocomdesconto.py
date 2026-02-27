d1a35fd735224090484a68fde975da1c279558be
Henriquecassol <168142302+henriquecassol09@users.noreply.github.com>
2026-02-27 11:07:36 -0300

    Add files via upload

--- pagamentocomdesconto.py
+++ pagamentocomdesconto.py
@@ -5,13 +5,11 @@ if text1 == 1:
     var1 = preco - (preco * 0.10)
     print(f"o valor a ser pago é {var1:g}")
 elif text1 == 2:
-        var2 = preco -(preco * 0.15)
-        print(f"o preço a ser pago é {var2:g}")
+    var2 = preco -(preco * 0.15)
+    print(f"o preço a ser pago é {var2:g}")
 elif text1 == 3:
-         var4 = preco
-         print(f"o valor a ser pago é {var4:g}")
-else: text1 == 4
-var3 = preco * 1.10
-print(input(f"o preço a ser pago é {var3:g}"))
-    
- 
+    var4 = preco
+    print(f"o valor a ser pago é {var4:g}")
+elif text1 == 4:
+    var3 = preco * 1.10
+    print(f"o preço a ser pago é {var3:g}")
