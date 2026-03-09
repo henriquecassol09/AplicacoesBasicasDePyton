a4484491bb2cda4bdca0571eb7aaacf004921bb2
Henriquecassol <168142302+henriquecassol09@users.noreply.github.com>
2026-03-09 10:37:56 -0300

    Add files via upload

---
+++ calculodousuario.py
@@ -0,0 +1,20 @@
+n1 = float (input("qual o primeiro numero? "))
+n2 = float (input("qual o segundo numero? "))
+operacao = int (input("para fazer a media entre os numeros digite 1, para a deferença dos numeros digite 2, para o produto dos numeros digite 3 e para a divição dos numeros digite 4 "))
+
+if operacao == 1:
+    r1 = (n1 + n2) / 2
+    print(f"a media entre os numeros é {r1:g}")
+elif operacao == 2:
+    r1 = n1 - n2
+    print(f"a diferença dos numeros é {r1:g}")
+elif operacao == 3:
+    r1 = n1 * n2
+    print(f"a produto dos numeros é {r1:g}")
+elif operacao == 4 and n2 != 0:
+    r1 = n1 / n2
+    print(f" a divisão dos numeros é {r1:g}")
+else:
+    print(f"você digitou um numero resultante de operação errada ou a operação é invalida mediante aos numeros")
+
+    #henriquecassol09
